0d6b98bc03134a005c3b380691dbc0d0ed215e4c
Glitch (frankyiu-api-headerparser) <none>
2020-08-04 12:48:57 +0000

    🌁🏓 Checkpoint ./server.js:19563985/36

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/whoami", function (req, res) {
+  console.log(req)
   res.json({greeting: 'hello API'});
 });
 
